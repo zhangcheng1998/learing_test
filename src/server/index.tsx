9e83418710576b75622d3cd1decab5a078b0bfb7
yongtao <yongtao.indigo@bytedance.com>
2023-03-08 14:31:23 +0800

    feat: 初始化启动

---
+++ src/server/index.tsx
@@ -0,0 +1,24 @@
+// ./src/server/index.tsx
+import express from "express";
+import childProcess from "child_process";
+import { renderToString } from "react-dom/server";
+import Home from "@/pages/Home";
+
+const app = express();
+const content = renderToString(<Home />);
+
+app.get("*", (req, res) => {
+  res.send(`
+    <html
+      <body>
+        <div>${content}</div>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log("ssr-server listen on 3000");
+});
+
+childProcess.exec("start http://127.0.0.1:3000");
